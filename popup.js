75a5be2115e93f570de9fac61e85246abd5e6be3
insanEvladi <insanEvladi@users.noreply.github.com>
2017-05-22 19:27:42 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -32,10 +32,46 @@ function l() {
 											method: "GET",
 											success: function(resp) {
 												term = (resp.match(/TERM_CAR\$/g) || []).length / 2 - 1;											
+												var obj = {};												
+												obj["ICAJAX"] = "1";
+												obj["ICNAVTYPEDROPDOWN"] = "1";
+												obj["ICType"] = "Panel";
+												obj["ICElementNum"] = "0";
+												obj["ICStateNum"] = "1";
+												obj["ICAction"] = "DERIVED_SSS_SCT_SSR_PB_GO";
+												obj["ICModelCancel"] = "0";
+												obj["ICXPos"] = "0";
+												obj["ICYPos"] = "0";
+												obj["ResponsetoDiffFrame"] = "-1";
+												obj["TargetFrameName"] = "None";
+												obj["FacetPath"] = "None";
+												obj["ICFocus"] = "";
+												obj["ICSaveWarningFilter"] = "0";
+												obj["ICChanged"] = "-1";
+												obj["ICAutoSave"] = "0";
+												obj["ICResubmit"] = "0";
+												obj["ICSID"] = "";
+												obj["ICActionPrompt"] = "false";
+												obj["ICTypeAheadID"] = "";
+												obj["ICBcDomData"] = "C~HC_SS_LAM_STD_GR_LST_GBL1~EMPLOYEE~HRMS~SA_LEARNER_SERVICES.SS_LAM_STD_GR_LST.GBL~UnknownValue~View%20My%20Assignments~UnknownValue~UnknownValue~https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fc%2FSA_LEARNER_SERVICES.SS_LAM_STD_GR_LST.GBL~UnknownValue*F~HCCC_ENROLLMENT~EMPLOYEE~HRMS~UnknownValue~UnknownValue~Enrollment~UnknownValue~UnknownValue~https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fs%2FWEBLIB_PT_NAV.ISCRIPT1.FieldFormula.IScript_PT_NAV_INFRAME%3Fpt_fname%3DHCCC_ENROLLMENT%26c%3D%26FolderPath%3DPORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT%26IsFolder%3Dtrue~UnknownValue*F~CO_EMPLOYEE_SELF_SERVICE~EMPLOYEE~HRMS~UnknownValue~UnknownValue~Self%20Service~UnknownValue~UnknownValue~https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fs%2FWEBLIB_PT_NAV.ISCRIPT1.FieldFormula.IScript_PT_NAV_INFRAME%3Fpt_fname%3DCO_EMPLOYEE_SELF_SERVICE%26c%3D%26FolderPath%3DPORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE%26IsFolder%3Dtrue~UnknownValue";
+												obj["ICPanelName"] = "";
+												obj["ICFind"] = "";
+												obj["ICAddCount"] = "";
+												obj["ICAPPCLSDATA"] = "";
+												obj["ICDataLang"] = "ENG";
+												obj["DERIVED_SSTSNAV_SSTS_MAIN_GOTO$7$"] = "9999";
+												obj["SSR_DUMMY_RECV1$sels$".concat(term.toString()).concat("$$0")] = term.toString();
+												obj["DERIVED_SSTSNAV_SSTS_MAIN_GOTO$8$"] = "9999";
+												obj["ptus_defaultlocalnode"] = "PSFT_HR";
+												obj["ptus_dbname"] = "KUPROD";
+												obj["ptus_portal"] = "EMPLOYEE";
+												obj["ptus_node"] = "HRMS";
+												obj["ptus_workcenterid"] = "";
+												obj["ptus_componenturl"] = "https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fc%2FSA_LEARNER_SERVICES.SS_LAM_STD_GR_LST.GBL";					
 												$.ajax({
 													url: "",
 													method: "POST",
-													data: { ICAJAX: "1", ICNAVTYPEDROPDOWN: "1", ICType: "Panel", ICElementNum: "0", ICStateNum: "1", ICAction: "DERIVED_SSS_SCT_SSR_PB_GO", ICModelCancel: "0", ICXPos: "0", ICYPos: "0", ResponsetoDiffFrame: "-1", TargetFrameName: "None", FacetPath: "None", ICFocus: "", ICSaveWarningFilter: "0", ICChanged: "-1", ICAutoSave: "0", ICResubmit: "0", ICSID: "", ICActionPrompt: "false", ICTypeAheadID: "", ICBcDomData: "C~HC_SS_LAM_STD_GR_LST_GBL1~EMPLOYEE~HRMS~SA_LEARNER_SERVICES.SS_LAM_STD_GR_LST.GBL~UnknownValue~View%20My%20Assignments~UnknownValue~UnknownValue~https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fc%2FSA_LEARNER_SERVICES.SS_LAM_STD_GR_LST.GBL~UnknownValue*F~HCCC_ENROLLMENT~EMPLOYEE~HRMS~UnknownValue~UnknownValue~Enrollment~UnknownValue~UnknownValue~https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fs%2FWEBLIB_PT_NAV.ISCRIPT1.FieldFormula.IScript_PT_NAV_INFRAME%3Fpt_fname%3DHCCC_ENROLLMENT%26c%3D%26FolderPath%3DPORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT%26IsFolder%3Dtrue~UnknownValue*F~CO_EMPLOYEE_SELF_SERVICE~EMPLOYEE~HRMS~UnknownValue~UnknownValue~Self%20Service~UnknownValue~UnknownValue~https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fs%2FWEBLIB_PT_NAV.ISCRIPT1.FieldFormula.IScript_PT_NAV_INFRAME%3Fpt_fname%3DCO_EMPLOYEE_SELF_SERVICE%26c%3D%26FolderPath%3DPORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE%26IsFolder%3Dtrue~UnknownValue", ICPanelName: "", ICFind: "", ICAddCount: "", ICAPPCLSDATA: "", ICDataLang: "ENG", DERIVED_SSTSNAV_SSTS_MAIN_GOTO$7$: "9999", "SSR_DUMMY_RECV1$sels$".concat(term.toString()).concat("$$0"): term, DERIVED_SSTSNAV_SSTS_MAIN_GOTO$8$: "9999", ptus_defaultlocalnode: "PSFT_HR", ptus_dbname: "KUPROD", ptus_portal: "EMPLOYEE", ptus_node: "HRMS", ptus_workcenterid: "", ptus_componenturl: "https%3A%2F%2Fkusis.ku.edu.tr%2Fpsp%2Fps%2FEMPLOYEE%2FHRMS%2Fc%2FSA_LEARNER_SERVICES.SS_LAM_STD_GR_LST.GBL" },
+													data: obj,
 													success: function(resp) {
 														$("#prog").css("width", 30 + "%");
 														var c = $(resp).find(".PSLEVEL1GRIDWBO tbody [valign='center']").size();
